101f2828550046f0dd4b540db1d50e9ec2bd23b0
andremedeiros99 <andremmedeiros99@gmail.com>
2024-08-24 22:49:55 -0300

    correcao na validacao

--- client_manager/forms.py
+++ client_manager/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Length
 
 class ClientForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=100)])
     cpf = StringField('CPF', validators=[DataRequired(), Length(min=11, max=11)])
     birth_date = DateField('Birth Date', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired()])
     submit = SubmitField('Submit')
